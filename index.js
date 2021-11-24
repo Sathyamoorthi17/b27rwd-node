d4a59e362ab01f423b8a14c1f97547d19f07b758
sathyamoorthi <sathyamoorthigopal@gmail.com>
2021-11-25 00:41:42 +0530

    PORT is changed (process.env.PORT)

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config();
 //Need middleware - all requests - apply the function
 app.use(express.json());
 
-const PORT = 9000;
+const PORT = process.env.PORT || 9000;
 
 // const MONGO_URL = "mongodb://localhost";
 const MONGO_URL = process.env.MONGO_URL;
